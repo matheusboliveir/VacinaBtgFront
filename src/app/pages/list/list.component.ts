981641bb310189f56117004b417026b53a206274
matheusboliveir <theus.braz18@gmail.com>
2025-08-26 18:25:45 -0300

    feature: pagina de listagem generica

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ColumnConfig, TableAction, TableProps } from '../../core/@types/List';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list.component.html',
-  styles: ``
+  styles: ``,
 })
-export class ListComponent {
+export class ListComponent<T> implements OnInit {
+  public title!: string;
+  public tableItems: T[] = [];
+  public tableColumns: ColumnConfig<T>[] = [];
+  public tableActions?: TableAction[] = [];
 
+  private route = inject(ActivatedRoute);
+
+  public ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      const table = data['table'] as TableProps<T>;
+      this.title = table.title;
+      this.tableActions = table.actions;
+      this.tableColumns = table.columns;
+      this.tableItems = table.items;
+    });
+  }
 }
