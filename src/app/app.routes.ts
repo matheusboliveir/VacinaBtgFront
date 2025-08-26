c248813b7fdf6d2008979269fb6188ee5ee93b25
matheusboliveir <theus.braz18@gmail.com>
2025-08-26 17:15:48 -0300

    chore: configuração inicial do bootstrap e rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'people',
+    component: ListComponent,
+  },
+  {
+    path: 'vacines',
+    component: ListComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'people',
+  },
+];
